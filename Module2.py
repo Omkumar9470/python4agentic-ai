e2a32212adfac2362e9607fde0d6b48f0e7d6396
Om Kumar <omkumar947062@gmail.com>
2026-06-26 13:16:08 +0530

    Token Counter

--- Module2.py
+++ Module2.py
@@ -1,3 +1,4 @@
+'''
 # Tokenization
 
 import tiktoken
@@ -23,3 +24,45 @@ print("Decoded : ", enc.decode(tokens2))
 print("Token ID: ", tokens3)
 print("Token Count: ", len(tokens3))
 print("Decoded : ", enc.decode(tokens3))
+
+'''
+
+# Token Counter
+
+import tiktoken
+
+def count_tokens (text: str, model: str = "gpt-4o") -> int:
+    enc = tiktoken.encoding_for_model(model)
+    return len(enc.encode(text))
+
+def count_message_tokens (messages: list) -> int:
+    enc = tiktoken.encoding_for_model("gpt-4o")
+    total = 0
+
+    for message in messages:
+        total += 4
+        for value in message.values():
+            total += len(enc.encode(value))
+    
+    total += 2
+    return total
+
+text = "Hello my name is Om Pandey"
+
+chat_history = [
+    {
+        "role": "system", 
+        "content": "You are a precise math tutor assistant."
+    },
+    {
+        "role": "user", 
+        "content": "What is 2 + 2?"
+    },
+    {
+        "role": "assistant", 
+        "content": "2 + 2 equals 4."
+    }
+]
+
+print(count_tokens(text))
+print(count_message_tokens(chat_history))
